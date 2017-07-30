e8469bc61e724c76ca1053f07373da9f3bfcd784
rahulsaty <rahul.pandey@3pillarglobal.com>
2017-07-30 11:52:42 +0530

    final commit

--- health+/app/src/main/java/health/tpg/com/health/activity/PatientReportListActivity.java
+++ health+/app/src/main/java/health/tpg/com/health/activity/PatientReportListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
